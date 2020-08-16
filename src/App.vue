f378732ee3a76459abc86d4feb6a9e5d28e4d3ff
leobrandoon <leo.gonzalez11@gmail.com>
2020-08-15 22:47:37 -0400

    problamas con el alert de error

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" class="container">
-    <Login />
+    
     <router-view />
   </div>
 </template>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,37 +1,54 @@
 <template>
-  <div class="jumbotron">
-    <h1 class="display-4">Welcome to The Casa Moscas</h1>
-    <p class="lead">postea lo que quieras sin muchas normas</p>
+  <div class="jumbotron card bg-dark text-white" id="avatar">
+    <div class="col">
+      <img src="@/assets/Mosca.png" alt width="100px" class id="avatar" />
+      <img src="@/assets/fondo.jpg" alt width="200px" class="rounded-circle" id="avatar" />
+      <h1 class="display-4">La Casa de las Moscas</h1>
+    </div>
+    <h2 class="lead">Onli Post de mierda</h2>
     <hr class="my-4" />
     <div class="row">
       <!-- aca va el formulario para resistro -->
-      <form class="col-sm">
+      <form class="col-sm" @submit.prevent="register">
         <div class="form-group">
-          <p>Si aun no eres parte de Casa Moscas, entonces registrate aqui!</p>
+          <p>Unete a otra red de mierda</p>
           <label>Email address</label>
-          <input type="email" class="form-control" required="required" v-model="login_email" />
+          <input type="email" class="form-control" required="required" v-model="register_email" />
         </div>
         <div class="form-group">
           <label>Password</label>
-          <input type="password" class="form-control" required="required" v-model="login_password" />
+          <input
+            id="alertapass"
+            type="password"
+            class="form-control"
+            required="required"
+            v-model="register_password"
+          />
         </div>
-
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <div class="form-group">
+          <label>Confirm your Password</label>
+          <input
+            type="password"
+            class="form-control"
+            required="required"
+            v-model="register_password_confirm"
+          />
+        </div>
+        <button type="submit" class="btn btn-primary">Mosqueate!</button>
       </form>
 
       <!-- aca va el formulario para login  -->
-      <form class="col-sm">
+      <form class="col-sm" @submit.prevent="login">
         <div class="form-group">
-          <p>Si ya estas registrado ingresa Aca !</p>
+          <p>Solo los que saben entran por aca.</p>
           <label>Email address</label>
-          <input type="email" class="form-control" />
+          <input type="email" class="form-control" v-model="login_email" />
         </div>
         <div class="form-group">
           <label>Password</label>
-          <input type="password" class="form-control" />
+          <input type="password" class="form-control" v-model="login_password" />
         </div>
-
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary">Entra al Vio!</button>
       </form>
     </div>
   </div>
@@ -45,14 +62,26 @@ export default {
     return {
       login_email: "",
       login_password: "",
+
+      register_email: "",
+      register_password: "",
+      register_password_confirm: "",
     };
   },
   methods: {
     login() {
       console.log("llegue");
     },
+    register() {
+      const alertapass = document.getElementByid("alertapass");
+      if (this.register_password != this.register_password_confirm) {
+        alertapass.setCustomValidity("se te mosquio la pass");
+        return;
+      }
+      // aca hacemos el registro
+    },
   },
 };
 </script>
-<style>
+<style scoped>
 </style>
